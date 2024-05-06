692d4b2419c727ef5785073cce23fda59be9cc3b
princesinghrajput <psr8084@gmail.com>
2024-05-06 11:04:23 +0530

    Implemented car models

--- car/models.py
+++ car/models.py
@@ -1,3 +1,97 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Car(models.Model):
+    state_choices = (
+        ('AN', 'Andaman and Nicobar Islands'),
+        ('AP', 'Andhra Pradesh'),
+        ('AR', 'Arunachal Pradesh'),
+        ('AS', 'Assam'),
+        ('BR', 'Bihar'),
+        ('CH', 'Chandigarh'),
+        ('CT', 'Chhattisgarh'),
+        ('DN', 'Dadra and Nagar Haveli and Daman and Diu'),
+        ('DL', 'Delhi'),
+        ('GA', 'Goa'),
+        ('GJ', 'Gujarat'),
+        ('HR', 'Haryana'),
+        ('HP', 'Himachal Pradesh'),
+        ('JK', 'Jammu and Kashmir'),
+        ('JH', 'Jharkhand'),
+        ('KA', 'Karnataka'),
+        ('KL', 'Kerala'),
+        ('LD', 'Ladakh'),
+        ('LA', 'Lakshadweep'),
+        ('MP', 'Madhya Pradesh'),
+        ('MH', 'Maharashtra'),
+        ('MN', 'Manipur'),
+        ('ML', 'Meghalaya'),
+        ('MZ', 'Mizoram'),
+        ('NL', 'Nagaland'),
+        ('OD', 'Odisha'),
+        ('PY', 'Puducherry'),
+        ('PB', 'Punjab'),
+        ('RJ', 'Rajasthan'),
+        ('SK', 'Sikkim'),
+        ('TN', 'Tamil Nadu'),
+        ('TS', 'Telangana'),
+        ('TR', 'Tripura'),
+        ('UP', 'Uttar Pradesh'),
+        ('UK', 'Uttarakhand'),
+        ('WB', 'West Bengal'),
+)
+
+    year_choices = [(r, r) for r in range(2000, datetime.now().year + 1)]
+
+    features_choices = (
+        ('Cruise Control', 'Cruise Control'),
+        ('Audio Interface', 'Audio Interface'),
+        ('Airbags', 'Airbags'),
+        ('Air Conditioning', 'Air Conditioning'),
+        ('Seat Heating', 'Seat Heating'),
+        ('Alarm System', 'Alarm System'),
+        ('ParkAssist', 'ParkAssist'),
+        ('Power Steering', 'Power Steering'),
+        ('Reversing Camera', 'Reversing Camera'),
+        ('Direct Fuel Injection', 'Direct Fuel Injection'),
+        ('Auto Start/Stop', 'Auto Start/Stop'),
+        ('Wind Deflector', 'Wind Deflector'),
+        ('Bluetooth Handset', 'Bluetooth Handset'),
+    )
+
+    door_choices = (
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+    )
+    car_title = models.CharField(max_length=255)
+    state = models.CharField(choices=state_choices, max_length=100)
+    city = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField(choices=year_choices)
+    condition = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description =models.TextField(max_length=300)
+    car_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    car_photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    features = models.CharField(max_length=100, choices=features_choices, blank=True, null=True)
+    body_style = models.CharField(max_length=100)
+    engine = models.CharField(max_length=100)
+    transmission = models.CharField(max_length=100)
+    interior = models.CharField(max_length=100)
+    miles = models.IntegerField()
+    doors = models.CharField(choices=door_choices, max_length=10)
+    passengers = models.IntegerField()
+    vin_no = models.CharField(max_length=100)
+    mileage = models.PositiveIntegerField(default=0)
+    fuel_type = models.CharField(max_length=50)
+    no_of_owners = models.CharField(max_length=100)
+    is_featured = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
