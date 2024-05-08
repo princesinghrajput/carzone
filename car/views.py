9a51834bf8e78ccb02c653289f9d2ef45ace01b5
princesinghrajput <psr8084@gmail.com>
2024-05-08 16:32:33 +0530

    Setup dedicated car page

--- car/views.py
+++ car/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from car.models import Car
 # Create your views here.
 def cars(request):
-    return render(request, 'car/cars.html')
+    cars = Car.objects.order_by("-created_date")
+    data = {
+        'cars': cars,
+    }
+    return render(request, 'car/cars.html', data)
+
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, pk=id) #take th primary key and get the data from the database
+    data = {
+        'single_car': single_car,
+    }
+    return render(request, 'car/car_detail.html',data)
+
